b044a412f01a97fc6f56528eb255eb099ae763ac
mzhariann <mzh.ariann@gmail.com>
2014-03-16 15:13:34 +0430

    insert without save

--- SAD/Insert.Designer.cs
+++ SAD/Insert.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.addStudent = new System.Windows.Forms.Button();
+            this.addProfessor = new System.Windows.Forms.Button();
+            this.addEvaluator = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // addStudent
@@ -41,11 +43,33 @@
             this.addStudent.UseVisualStyleBackColor = true;
             this.addStudent.Click += new System.EventHandler(this.addStudent_Click);
             // 
+            // addProfessor
+            // 
+            this.addProfessor.Location = new System.Drawing.Point(57, 100);
+            this.addProfessor.Name = "addProfessor";
+            this.addProfessor.Size = new System.Drawing.Size(182, 23);
+            this.addProfessor.TabIndex = 1;
+            this.addProfessor.Text = "اضافه کردن استاد";
+            this.addProfessor.UseVisualStyleBackColor = true;
+            this.addProfessor.Click += new System.EventHandler(this.addProfessor_Click);
+            // 
+            // addEvaluator
+            // 
+            this.addEvaluator.Location = new System.Drawing.Point(57, 129);
+            this.addEvaluator.Name = "addEvaluator";
+            this.addEvaluator.Size = new System.Drawing.Size(182, 23);
+            this.addEvaluator.TabIndex = 2;
+            this.addEvaluator.Text = "اضافه کردن ارزیاب";
+            this.addEvaluator.UseVisualStyleBackColor = true;
+            this.addEvaluator.Click += new System.EventHandler(this.addEvaluator_Click);
+            // 
             // Insert
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.addEvaluator);
+            this.Controls.Add(this.addProfessor);
             this.Controls.Add(this.addStudent);
             this.Name = "Insert";
             this.Text = "ویرایش";
@@ -56,5 +80,7 @@
         #endregion
 
         private System.Windows.Forms.Button addStudent;
+        private System.Windows.Forms.Button addProfessor;
+        private System.Windows.Forms.Button addEvaluator;
     }
 }
--- SAD/Insert.cs
+++ SAD/Insert.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+    
 namespace SAD
 {
     public partial class Insert : Form
@@ -18,24 +18,32 @@ namespace SAD
         }
         private void addStudent_Click(object sender, EventArgs e)
         {
-            addTextBox("شماره دانشجویی", 0, 0, 100, 5);
+            addTextBox("شماره‌ی دانشجویی", 0, 0, 100, 5);
             addTextBox("نام", 0, 25, 100, 30);
             addTextBox("نام خانوادگی", 0, 50, 100, 55);
             addTextBox("ایمیل", 0, 75, 100, 80);
             addTextBox("مقطع", 0, 100, 100, 105);
             addTextBox("سال ورود", 0, 125, 100, 130);
             addTextBox("رشته", 0, 150, 100, 155);
-            addTextBox("استاد راهنما", 0, 175, 100,180);
-
-            Button save = new Button();
-            save.Text = "ذخیره";
-            save.Location = new Point(150, 220);
-            save.Click += new System.EventHandler(save_Click);
-            this.Controls.Add(save);
-            this.Controls.Remove(addStudent);
+            addTextBox("استاد راهنما", 0, 175, 100, 180);    
+            addSaveButton();
+         
         }
         private void save_Click(Object sender, System.EventArgs e)
         {
+            if (this.Controls[1].Text.Equals("شماره‌ی دانشجویی"))
+            {
+                Student s = new Student();
+                s.Id = int.Parse(Controls[0].Text);
+                s.fname = Controls[2].Text;
+                s.lname = Controls[4].Text;
+                s.email = Controls[6].Text;
+                s.degree = int.Parse(Controls[8].Text);
+                s.entranceYear = int.Parse(Controls[10].Text);
+                s.majorId = int.Parse(Controls[12].Text);
+               // s.supervisor = Controls[14].Text;
+            }
+
         }
         private void addTextBox(string text , int x1, int y1, int x2, int y2)
         {
@@ -46,6 +54,38 @@ namespace SAD
             dynamicL.Location = new Point(x2, y2);
             this.Controls.Add(dynamicTB);
             this.Controls.Add(dynamicL);
+        }
+
+        private void addSaveButton()
+        {
+            Button save = new Button();
+            save.Text = "ذخیره";
+            save.Location = new Point(150, 220);
+            save.Click += new System.EventHandler(save_Click);
+            this.Controls.Add(save);
+            this.Controls.Remove(addStudent);
+            this.Controls.Remove(addProfessor);
+            this.Controls.Remove(addEvaluator);
+
+        }
+        private void addProfessor_Click(object sender, EventArgs e)
+        {
+            addTextBox("شماره‌ی پرسنلی", 0, 0, 100, 5);
+            addTextBox("نام", 0, 25, 100, 30);
+            addTextBox("نام خانوادگی", 0, 50, 100, 55);
+            addTextBox("ایمیل", 0, 75, 100, 80);
+            addTextBox("مدیر گروه", 0, 100, 100, 105);
+            addSaveButton();
+        }
+
+        private void addEvaluator_Click(object sender, EventArgs e)
+        {
+            addTextBox("شماره‌ی ارزیابی", 0, 0, 100, 5);
+            addTextBox("نام", 0, 25, 100, 30);
+            addTextBox("نام خانوادگی", 0, 50, 100, 55);
+            addTextBox("ایمیل", 0, 75, 100, 80);
+            addSaveButton();
+
         }
     }
 }
