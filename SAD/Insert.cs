d8bd0fd99410d017b6ab695dddfb0e99f4508f0a
mzhariann <mzh.ariann@gmail.com>
2014-03-16 11:19:26 +0430

    Tamiz code

--- SAD/Insert.cs
+++ SAD/Insert.cs
@@ -16,83 +16,36 @@ namespace SAD
         {
             InitializeComponent();
         }
-        string test;
         private void addStudent_Click(object sender, EventArgs e)
         {
-            TextBox dynamicTB = new TextBox();
-            dynamicTB.Location = new Point(0, 0);
-            Label dynamicL = new Label();
-            dynamicL.Text = "شماره‌ی دانشجویی";
-            dynamicL.Location = new Point(100, 5);
-
-            TextBox dynamicTB2 = new TextBox();
-            dynamicTB2.Location = new Point(0, 25);
-            Label dynamicL2 = new Label();
-            dynamicL2.Text = "نام";
-            dynamicL2.Location = new Point(100, 30);
-
-            TextBox dynamicTB3 = new TextBox();
-            dynamicTB3.Location = new Point(0, 50);
-            Label dynamicL3 = new Label();
-            dynamicL3.Text = "نام خانوادگی";
-            dynamicL3.Location = new Point(100, 55);
-
-            TextBox dynamicTB4 = new TextBox();
-            dynamicTB4.Location = new Point(0, 75);
-            Label dynamicL4 = new Label();
-            dynamicL4.Text = "پست الکترونیکی";
-            dynamicL4.Location = new Point(100, 80);
-
-            TextBox dynamicTB5 = new TextBox();
-            dynamicTB5.Location = new Point(0, 100);
-            Label dynamicL5 = new Label();
-            dynamicL5.Text = "مقطع";
-            dynamicL5.Location = new Point(100, 105);
-
-            TextBox dynamicTB6 = new TextBox();
-            dynamicTB6.Location = new Point(0, 125);
-            Label dynamicL6 = new Label();
-            dynamicL6.Text = "سال ورود";
-            dynamicL6.Location = new Point(100, 130);
-
-            TextBox dynamicTB7 = new TextBox();
-            dynamicTB7.Location = new Point(0, 150);
-            Label dynamicL7 = new Label();
-            dynamicL7.Text = "رشته";
-            dynamicL7.Location = new Point(100, 155);
-
-            TextBox dynamicTB8 = new TextBox();
-            dynamicTB8.Location = new Point(0, 175);
-            Label dynamicL8 = new Label();
-            dynamicL8.Text = "استاد راهنما";
-            dynamicL8.Location = new Point(100, 180);
+            addTextBox("شماره دانشجویی", 0, 0, 100, 5);
+            addTextBox("نام", 0, 25, 100, 30);
+            addTextBox("نام خانوادگی", 0, 50, 100, 55);
+            addTextBox("ایمیل", 0, 75, 100, 80);
+            addTextBox("مقطع", 0, 100, 100, 105);
+            addTextBox("سال ورود", 0, 125, 100, 130);
+            addTextBox("رشته", 0, 150, 100, 155);
+            addTextBox("استاد راهنما", 0, 175, 100,180);
 
             Button save = new Button();
             save.Text = "ذخیره";
             save.Location = new Point(150, 220);
             save.Click += new System.EventHandler(save_Click);
-            this.Controls.Add(dynamicL);
-            this.Controls.Add(dynamicTB);
-            this.Controls.Add(dynamicL2);
-            this.Controls.Add(dynamicTB2);
-            this.Controls.Add(dynamicL3);
-            this.Controls.Add(dynamicTB3);
-            this.Controls.Add(dynamicL4);
-            this.Controls.Add(dynamicTB4);
-            this.Controls.Add(dynamicL5);
-            this.Controls.Add(dynamicTB5);
-            this.Controls.Add(dynamicL6);
-            this.Controls.Add(dynamicTB6);
-            this.Controls.Add(dynamicL7);
-            this.Controls.Add(dynamicTB7);
-            this.Controls.Add(dynamicL8);
-            this.Controls.Add(dynamicTB8);
-
             this.Controls.Add(save);
             this.Controls.Remove(addStudent);
         }
         private void save_Click(Object sender, System.EventArgs e)
         {
+        }
+        private void addTextBox(string text , int x1, int y1, int x2, int y2)
+        {
+            TextBox dynamicTB = new TextBox();
+            dynamicTB.Location = new Point(x1, y1);
+            Label dynamicL = new Label();
+            dynamicL.Text = text;
+            dynamicL.Location = new Point(x2, y2);
+            this.Controls.Add(dynamicTB);
+            this.Controls.Add(dynamicL);
         }
     }
 }
