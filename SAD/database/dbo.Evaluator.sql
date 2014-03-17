425b03c912804356abd0304767764adb9adfc50a
yahrabi <yasi.ahrabi@yahoo.com>
2014-03-17 10:18:09 +0430

    DB ke task dare + person Id

--- SAD/database/dbo.Evaluator.sql
+++ SAD/database/dbo.Evaluator.sql
@@ -1,7 +1,9 @@
-﻿CREATE TABLE [dbo].[Evaluator]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [email] VARCHAR(50) NULL, 
-    [fName] VARCHAR(50) NULL, 
-    [lName] VARCHAR(50) NULL
-)
+﻿CREATE TABLE [dbo].[Evaluator] (
+    [Id]    INT          NOT NULL,
+    [email] VARCHAR (50) NULL,
+    [fName] VARCHAR (50) NULL,
+    [lName] VARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+	FOREIGN KEY ([Id]) REFERENCES [dbo].[Person] ([Id]),
+);
+
--- SAD/database/dbo.Student.sql
+++ SAD/database/dbo.Student.sql
@@ -1,11 +1,15 @@
-﻿CREATE TABLE [dbo].[Student]
-(
-	[SID] INT NOT NULL PRIMARY KEY, 
-    [fname] VARCHAR(50) NULL, 
-    [lname] VARCHAR(50) NULL, 
-    [email] VARCHAR(50) NULL, 
-    [degree] INT NULL, 
-    [entranceYear] INT NULL, 
-    [major] INT NULL
-	foreign key(major) references major
-)
+﻿CREATE TABLE [dbo].[Student] (
+    [Id]           INT          NOT NULL,
+    [fname]        VARCHAR (50) NULL,
+    [lname]        VARCHAR (50) NULL,
+    [email]        VARCHAR (50) NULL,
+    [degree]       INT          NULL,
+    [entranceYear] INT          NULL,
+    [majorId]      INT          NULL,
+    [supervisor]   INT          NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([majorId]) REFERENCES [dbo].[Major] ([Id]),
+    FOREIGN KEY ([supervisor]) REFERENCES [dbo].[Professor] ([Id]),
+	FOREIGN KEY ([Id]) REFERENCES [dbo].[Person] ([Id]),
+);
+
