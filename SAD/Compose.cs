2a351db7111e99320aee0627a2f3da62efdb5602
Une30 <younesi429@yahoo.com>
2014-03-17 12:07:59 +0430

    UIs

---
+++ SAD/Compose.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SAD
+{
+    public partial class Compose : Form
+    {
+        public Compose()
+        {
+            InitializeComponent();
+        }
+
+        private void editor_Text_Changed(object sender, EventArgs e)
+        {
+
+        }
+
+        private void save_Button_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SAD/Home.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SAD
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void student_Info_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void prof_Info_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void eval_Prof_Info_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void events_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reports_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void log_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void confirm_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void new_Task_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
