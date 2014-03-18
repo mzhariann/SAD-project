99874297cde3af89c02befe24ceafe4d0846cce1
mzhariann <mzh.ariann@gmail.com>
2014-03-19 02:21:10 +0430

    task

---
+++ SAD/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAD
+{
+    public class Task
+    {
+        public string subject { set; get; }
+        public string date { set; get; }
+        public string content { set; get; }
+        public string confirmation { set; get; }
+        public string sendingOption { set; get; }
+        public List<string> receivers { set; get; }
+    }
+}
