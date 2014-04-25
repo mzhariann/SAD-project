b9fa375ab8f149bed19c1f52cdd0f9c0b740aac7
yahrabi <yasi.ahrabi@yahoo.com>
2014-04-25 15:35:13 +0430

    dard maraz kooft

---
+++ SAD/MailCreator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAD
+{
+    public class MailCreator
+    {
+
+        MailCreator() { 
+        
+        }
+    }
+}
---
+++ SAD/TemplatePopulater.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace SAD
+{
+    public static class TemplatePopulater
+    {
+        
+        public static void populate(int id , string table, TemplateFile temp) {
+            string[] columns = dbConnection.getTableColumns(table);
+            string tempText = temp.getText();
+
+            foreach (string col in columns)
+            {
+                string pattern = "<@" + table + "." + col + ">";
+                string replacement = dbConnection.getField(table , id , col);
+                Regex rgx = new Regex(pattern);
+                tempText = rgx.Replace(tempText, replacement);
+            }
+            temp.setText(tempText);
+        }
+
+
+    }
+}
