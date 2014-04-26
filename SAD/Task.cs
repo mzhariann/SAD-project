5f8f99ceb1b28fc417cd8969f34dce8f588e6d96
mzhariann <mzh.ariann@gmail.com>
2014-04-26 19:36:15 +0430

    task, updated with edit option and a better view

--- SAD/Task.cs
+++ SAD/Task.cs
@@ -9,10 +9,12 @@ namespace SAD
     public class Task
     {
         public string subject { set; get; }
-        public string date { set; get; }
+        public DateTime creationDate { set; get; }
         public string content { set; get; }
-        public string confirmation { set; get; }
-        public string sendingOption { set; get; }
+        public Boolean confirmation { set; get; }
+        public PeriodicSend prdSend { set; get; }
+        public DateTime sendingDate { set; get; }
         public List<string> receivers { set; get; }
+        public Boolean periodicSend = false;
     }
 }
