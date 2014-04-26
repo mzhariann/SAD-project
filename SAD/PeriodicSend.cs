8e675e54e530a3152d479d344ac96ba2236ded28
mzhariann <mzh.ariann@gmail.com>
2014-04-26 20:23:49 +0430

    sad

---
+++ SAD/PeriodicSend.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAD
+{
+    
+    public class PeriodicSend
+    {
+        public DateTime startDate { set; get; }
+        public DateTime endDate { set; get; }
+        public KeyValuePair<int,int > period { set; get; }
+    }
+    public enum Periods {Week, Month, Year};
+}
