b72ce5fa3d464facccdf0b4eea4e246dc0de65b3
mzhariann <mzh.ariann@gmail.com>
2014-04-27 19:38:35 +0430

    task

--- SAD/Task.cs
+++ SAD/Task.cs
@@ -3,18 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+
 
 namespace SAD
 {
     public class Task
     {
+        public int id { set; get; }
         public string subject { set; get; }
         public DateTime creationDate { set; get; }
         public string content { set; get; }
         public Boolean confirmation { set; get; }
         public PeriodicSend prdSend { set; get; }
         public DateTime sendingDate { set; get; }
-        public List<string> receivers { set; get; }
+        public List<int> receivers { set; get; }
         public Boolean periodicSend = false;
+        public Task()
+        {
+            prdSend = new PeriodicSend();
+            receivers = new List<int>();
+        }
+        public void saveToDB()
+        {
+            TaskDAO.insert(this);
+        }
+        public void updateInDB()
+        {
+        }
+        public void deleteFromDB()
+        {
+            TaskDAO.delete(this);
+        }
+        public void addReceivers()
+        {
+            TaskDAO.insertReceivers(this);
+        }
+        public static DataTable getSubjectTable()
+        {           
+            return TaskDAO.getSubjectTable();
+        }
+        public static Task getById(int id)
+        {
+            return TaskDAO.getById(id);
+        }
     }
 }
