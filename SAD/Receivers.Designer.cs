8d241b8a8a0772da05484eb05b932cccba7ccd1f
mzhariann <mzh.ariann@gmail.com>
2014-04-27 19:40:43 +0430

    receivers

---
+++ SAD/Receivers.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace SAD
+{
+    partial class Receivers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpsGridView = new System.Windows.Forms.DataGridView();
+            this.addGrps = new System.Windows.Forms.Button();
+            this.showMembers = new System.Windows.Forms.Button();
+            this.done = new System.Windows.Forms.Button();
+            this.showGrps = new System.Windows.Forms.Button();
+            this.addMembers = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grpsGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grpsGridView
+            // 
+            this.grpsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grpsGridView.Location = new System.Drawing.Point(12, 33);
+            this.grpsGridView.Name = "grpsGridView";
+            this.grpsGridView.Size = new System.Drawing.Size(727, 170);
+            this.grpsGridView.TabIndex = 0;
+            // 
+            // addGrps
+            // 
+            this.addGrps.Location = new System.Drawing.Point(443, 209);
+            this.addGrps.Name = "addGrps";
+            this.addGrps.Size = new System.Drawing.Size(181, 23);
+            this.addGrps.TabIndex = 2;
+            this.addGrps.Text = "اضافه کردن گروه‌های انتخاب شده";
+            this.addGrps.UseVisualStyleBackColor = true;
+            this.addGrps.Click += new System.EventHandler(this.addGrps_Click);
+            // 
+            // showMembers
+            // 
+            this.showMembers.Location = new System.Drawing.Point(246, 209);
+            this.showMembers.Name = "showMembers";
+            this.showMembers.Size = new System.Drawing.Size(191, 23);
+            this.showMembers.TabIndex = 3;
+            this.showMembers.Text = "مشاهده‌ی اعضای گروه انتخاب شده";
+            this.showMembers.UseVisualStyleBackColor = true;
+            this.showMembers.Click += new System.EventHandler(this.showMembers_Click);
+            // 
+            // done
+            // 
+            this.done.Location = new System.Drawing.Point(263, 257);
+            this.done.Name = "done";
+            this.done.Size = new System.Drawing.Size(207, 23);
+            this.done.TabIndex = 4;
+            this.done.Text = "اتمام انتخاب";
+            this.done.UseVisualStyleBackColor = true;
+            this.done.Click += new System.EventHandler(this.done_Click);
+            // 
+            // showGrps
+            // 
+            this.showGrps.Location = new System.Drawing.Point(630, 209);
+            this.showGrps.Name = "showGrps";
+            this.showGrps.Size = new System.Drawing.Size(109, 23);
+            this.showGrps.TabIndex = 5;
+            this.showGrps.Text = "مشاهده‌ی گروه‌ها";
+            this.showGrps.UseVisualStyleBackColor = true;
+            this.showGrps.Click += new System.EventHandler(this.showGrps_Click);
+            // 
+            // addMembers
+            // 
+            this.addMembers.Location = new System.Drawing.Point(12, 209);
+            this.addMembers.Name = "addMembers";
+            this.addMembers.Size = new System.Drawing.Size(228, 23);
+            this.addMembers.TabIndex = 6;
+            this.addMembers.Text = "اضافه کردن افراد انتخاب شده";
+            this.addMembers.UseVisualStyleBackColor = true;
+            this.addMembers.Click += new System.EventHandler(this.addMembers_Click);
+            // 
+            // Receivers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 294);
+            this.Controls.Add(this.addMembers);
+            this.Controls.Add(this.showGrps);
+            this.Controls.Add(this.done);
+            this.Controls.Add(this.showMembers);
+            this.Controls.Add(this.addGrps);
+            this.Controls.Add(this.grpsGridView);
+            this.Name = "Receivers";
+            this.Text = "Receivers";
+            ((System.ComponentModel.ISupportInitialize)(this.grpsGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grpsGridView;
+        private System.Windows.Forms.Button addGrps;
+        private System.Windows.Forms.Button showMembers;
+        private System.Windows.Forms.Button done;
+        private System.Windows.Forms.Button showGrps;
+        private System.Windows.Forms.Button addMembers;
+    }
+}
---
+++ SAD/Receivers.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SAD
+{
+    public partial class Receivers : Form
+    {
+        Task task;
+        public Receivers(Task tsk)
+        {
+            InitializeComponent();
+            task = tsk;
+            Controls.OfType<Button>().First(x => x.Name == "showGrps").Enabled = true;
+            Controls.OfType<Button>().First(x => x.Name == "addGrps").Enabled = false;
+            Controls.OfType<Button>().First(x => x.Name == "showMembers").Enabled = false;
+            Controls.OfType<Button>().First(x => x.Name == "addMembers").Enabled = false;
+
+        }
+
+        List<int> selectedGrpsIds = new List<int>();
+        private void addGrps_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < grpsGridView.SelectedRows.Count; i++)
+                selectedGrpsIds.Add((int)grpsGridView.Rows[grpsGridView.SelectedRows[i].Index].Cells["Id"].Value);
+            MessageBox.Show("گروه مورد نظر به لیست گیرنده‌ها اضافه شد");
+        }
+        private void showMembers_Click(object sender, EventArgs e)
+        {
+            grpsGridView.DataSource = dbConnection.getMembersInGroup((int)grpsGridView.Rows[grpsGridView.SelectedRows[0].Index].Cells["Id"].Value);
+            Controls.OfType<Button>().First(x => x.Name == "showGrps").Enabled = true;
+            Controls.OfType<Button>().First(x => x.Name == "addGrps").Enabled = false;
+            Controls.OfType<Button>().First(x => x.Name == "showMembers").Enabled = false;
+            Controls.OfType<Button>().First(x => x.Name == "addMembers").Enabled = true;
+
+
+        }
+
+        List<int> selectedPIds = new List<int>();
+
+        private void done_Click(object sender, EventArgs e)
+        {
+            task.receivers = new List<int>();
+            task.receivers.AddRange(selectedPIds);
+            
+            for (int i = 0; i < selectedGrpsIds.Count; i++)
+            {
+                DataTable d = dbConnection.getMembersInGroup(selectedGrpsIds[i]);
+                task.receivers.AddRange( (from row in d.AsEnumerable()
+         select row.Field<int>("Id")).ToList<int>());
+            }
+            this.Close();
+        }
+
+        private void showGrps_Click(object sender, EventArgs e)
+        {
+            DataTable t = dbConnection.getGroupsTable();
+            grpsGridView.DataSource = t;
+            Controls.OfType<Button>().First(x => x.Name == "showGrps").Enabled = false;
+            Controls.OfType<Button>().First(x => x.Name == "addGrps").Enabled = true;
+            Controls.OfType<Button>().First(x => x.Name == "showMembers").Enabled = true;
+            Controls.OfType<Button>().First(x => x.Name == "addMembers").Enabled = false;
+
+        }
+
+        private void addMembers_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < grpsGridView.SelectedRows.Count; i++)
+                selectedPIds.Add((int)grpsGridView.Rows[grpsGridView.SelectedRows[i].Index].Cells["Id"].Value);
+            MessageBox.Show("افراد مورد نظر به لیست گیرنده‌ها اضافه شد");
+        }
+    }
+}
